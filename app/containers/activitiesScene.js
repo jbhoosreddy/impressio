e9ecb663afd44bd7e8907372e7f775d42f495a7b
helfi92 <helfi92@gmail.com>
2016-10-01 16:57:15 -0400

    add image button, caption input and tags input

--- app/containers/activitiesScene.js
+++ app/containers/activitiesScene.js
@@ -2,19 +2,57 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableOpacity,
   Navigator,
   StyleSheet
 } from 'react-native';
 
 export default class Activities extends Component {
   static get defaultProps() {
-    return { title: 'MyScene' };
+    return {
+      title: 'MyScene'
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.addImage = this.addImage.bind(this);
+
+    this.state = {
+      caption: '',
+      tags: ''
+    }
+  }
+
+  addImage() {
+    console.log('add image onclick');
   }
 
   render() {
+    const { caption, tags } = this.state;
     return (
       <View style={styles.tabContent}>
-        <Text style={styles.tabText}>Jaideeeep {this.props.title}</Text>
+        <Text style={styles.label}>{this.props.title}</Text>
+
+        <TouchableOpacity style={styles.buttonWrapper} onPress={() => this.addImage()}>
+          <Text>Add Image</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.label}>Caption</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(caption) => this.setState({ caption })}
+          value={caption}
+        />
+
+        <Text style={styles.label}>Tags (Seperated by commas)</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(tags) => this.setState({ tags })}
+          value={tags}
+        />
       </View>
     );
   }
@@ -23,10 +61,26 @@ export default class Activities extends Component {
 const styles = StyleSheet.create({
   tabContent: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  tabText: {
-    margin: 50,
-    fontSize: 45
+  label: {
+    marginTop: 20,
+    fontSize: 20
+  },
+  input: {
+    marginTop: 20,
+    fontSize: 45,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  buttonWrapper: {
+    backgroundColor: '#03C799',
+    height: 46,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    padding: 20,
+    overflow: 'hidden',
   }
 });
